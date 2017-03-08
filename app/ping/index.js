e073e1307990d369848a8446b401c10be303e425
Joseph Thibeault <joseph.thibeault@gmail.com>
2017-03-08 16:47:08 -0500

    refactor(routes, ping): change subrouter mount path in main router

--- app/ping/index.js
+++ app/ping/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 const logger = require('shared/logger').child({ module: 'ping' });
 
-router.get('/ping', function(req, res) {
+router.get('/', function(req, res) {
     res.status(200).json({});
 });
 
--- app/router.js
+++ app/router.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const drErrorHandler = require('core/middleware/error-handler');
 
 // TODO: mount sub-routers here, if needed.
-router.use(require('./ping'));
+router.use('/ping', require('./ping'));
 
 router.use(drErrorHandler);
 
